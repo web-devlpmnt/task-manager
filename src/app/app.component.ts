ea5271433a09d7f185a8ecbc68565abf2ddc16fb
fmagalhaes <fernando.rj021@hotmail.com>
2024-05-16 19:02:37 -0300

    Initial Layout Testing in app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'task-manager';
+  faTimes = faTimes;
 }
